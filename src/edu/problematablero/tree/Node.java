b7639f05c300fc0b7eaf7337d49174848847b0ab
cbellom <cbellom@unal.edu.co>
2014-03-24 16:29:39 -0500

    Update Node.java
    izq, arriba, abajo

--- src/edu/problematablero/tree/Node.java
+++ src/edu/problematablero/tree/Node.java
@@ -25,7 +25,7 @@ public class Node {
         tablero = data;
     }
     
-    public Node getDerecha(){
+    public Node setDerecha(){
         int i = 0,j = 0;
         while(tablero[i][j] != 0){
             i++;
@@ -40,6 +40,57 @@ public class Node {
         ret[i+1][j] = 0;
         derecha = new Node(ret);
         return derecha;
+    }
+    
+      public Node setIzquierda(){
+        int i = 0,j = 0;
+        while(tablero[i][j] != 0){
+            i++;
+            if(i==3){
+                i=0;
+                j++;
+            }
+        }
+        if(i==0) return null;
+        int[][] ret= Arrays.copyOf(tablero, tablero.length);
+        ret[i][j] = ret[i-1][j];
+        ret[i-1][j] = 0;
+        izquierda = new Node(ret);
+        return izquierda;
+    }
+    
+    public Node setArriba(){
+        int i = 0,j = 0;
+        while(tablero[i][j] != 0){
+            i++;
+            if(i==3){
+                i=0;
+                j++;
+            }
+        }
+        if(j==0) return null;
+        int[][] ret= Arrays.copyOf(tablero, tablero.length);
+        ret[i][j] = ret[i][j-1];
+        ret[i][j-1] = 0;
+        arriba = new Node(ret);
+        return arriba;
+    }
+    
+    public Node setAbajo(){
+        int i = 0,j = 0;
+        while(tablero[i][j] != 0){
+            i++;
+            if(i==3){
+                i=0;
+                j++;
+            }
+        }
+        if(j==2) return null;
+        int[][] ret= Arrays.copyOf(tablero, tablero.length);
+        ret[i][j] = ret[i][j+1];
+        ret[i][j+1] = 0;
+        abajo = new Node(ret);
+        return abajo;
     }
     
 }
